60cfa9e619f7fc26a3b9e4ceab0814207d28c1e2
alperozc <48479234+alperozc@users.noreply.github.com>
2022-12-29 01:56:44 +0300

    v0.11

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDTO, UpdateRoleDTO } from './dto/roles.dto';
+import { hasRoles } from './roles.decorator';
+import JwtGuard from 'src/auth/jwt.guard';
+import { RolesGuard } from './roles.guard';
 
 @Controller('roles')
 export class RolesController {
@@ -16,18 +19,24 @@ export class RolesController {
         return this.rolesService.getRole(id);
     }
 
+    @hasRoles('SYSTEM_ADMIN', 'HUMAN_RESOURCES')
+    @UseGuards(JwtGuard, RolesGuard)
     @Post()
     @UsePipes(ValidationPipe)
     createRole(@Body() roleDTO: CreateRoleDTO) {
         return this.rolesService.createRole(roleDTO)
     }
 
+    @hasRoles('SYSTEM_ADMIN', 'HUMAN_RESOURCES')
+    @UseGuards(JwtGuard, RolesGuard)
     @Put(':id')
     @UsePipes(ValidationPipe)
     updateRole(@Body() roleDTO: UpdateRoleDTO, @Param('id') id: number) {
         return this.rolesService.updateRole(id, roleDTO)
     }
 
+    @hasRoles('SYSTEM_ADMIN', 'HUMAN_RESOURCES')
+    @UseGuards(JwtGuard, RolesGuard)
     @Delete(':id')
     deleteRole(@Param('id') id: number) {
         return this.rolesService.deleteRole(id).then(() => 'Role deleted')
